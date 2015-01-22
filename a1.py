cf66fee39dd11ec4b65876f82f8f962c5a8630a3
Ben DeCoste <neb@goinstant.com>
2015-01-21 23:30:25 -0400

    naming, downloading, uploading, listing all down
    now make it pretty

--- a1.py
+++ a1.py
@@ -1,8 +1,16 @@
+import os
+import shutil
+
 from Tkinter import *
 import tkFileDialog
+from PIL import Image, ImageTk
+
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+
+import json
+import wget
 
 class App:
 
@@ -31,12 +39,22 @@ class App:
     self.url_upload_btn = Button(frame, text="Upload URL", command=self.upload_url)
     self.url_upload_btn.grid(row=2, column=1, sticky=E)
 
+    Label(frame, text="Enter Unique Name").grid(row=3, column=0, sticky=E, padx=30)
+    self.file_name_string = StringVar()
+    self.file_name_entry = Entry(frame, textvariable=self.file_name_string)
+    self.file_name_entry.grid(row=3, column=1, sticky=E, padx=30)
+
     # images
-    self.image_listbox = Listbox(frame)
-    self.image_listbox.grid(row=3)
+    scrollbar = Scrollbar(frame, orient=VERTICAL)
+    self.image_listbox = Listbox(frame, yscrollcommand=scrollbar.set)
+    scrollbar.config(command=self.image_listbox.yview)
+    # scrollbar.pack(side=RIGHT, fill=Y)
+    self.image_listbox.bind('<Double-Button-1>', self.download_file)
+    self.image_listbox.grid(row=4)
+
+    self.image_map = {}
 
     self.refresh_images()
-
 
     ## File options
     self.file_opt = {}
@@ -47,12 +65,25 @@ class App:
         ];
 
 
+
+  def download_file(self, event):
+    img = self.image_listbox.get(self.image_listbox.curselection()[0])
+    dl_url = self.image_map[img]
+    print "downloading: " + dl_url
+
+    if not os.path.exists('downloads'):
+      print "creating download directory"
+      os.makedirs('downloads')
+
+    wget.download(dl_url, out='downloads/')
+
   def upload_file(self):
 
       f = tkFileDialog.askopenfilename(**self.file_opt)
       print "Uploading file (" + f + ")"
 
-      cloudinary.uploader.upload(f)
+      cloudinary.uploader.upload(f, public_id=self.file_name_string.get())
+      self.refresh_images
 
   def upload_url(self):
 
@@ -61,10 +92,37 @@ class App:
       cloudinary.uploader.upload(url)
       print "uploaded url image"
 
+  def __refresh_images(self, next_c):
+    all_res = None
+    if next_c is not None:
+      all_res = cloudinary.api.resources(next_cursor=next_c)
+    else:
+      all_res = cloudinary.api.resources()
+
+    for res in all_res['resources']:
+      self.image_map[res['public_id']] = res['url']
+      self.image_listbox.insert(END, res['public_id'])
+
+    if 'next_cursor' in all_res:
+      self.__refresh_images(all_res['next_cursor'])
+
   def refresh_images(self):
-      resources = cloudinary.api.resources()
-      print resources
+      self.__refresh_images(None)
+
+      # if not os.path.exists('tmp'):
+      #   print "making tmp dir"
+      #   os.makedirs('tmp')
+
+      # for res in all_res['resources']:
+      #   wget.download(res['url'], out='tmp/')
+
+      # for img in os.listdir('tmp'):
+      #   # self.image_listbox.insert(END, PhotoImage(file='tmp/' + img))
+      #   loaded_image = Image.open('tmp/' + img)
+      #   self.image_listbox.insert(END, ImageTk.PhotoImage(file='tmp/' + img))
 
 root = Tk()
+root.geometry("%dx%d+%d+%d" % (600, 400, 100, 100))
 app = App(root)
 root.mainloop()
+# shutil.rmtree('tmp')
